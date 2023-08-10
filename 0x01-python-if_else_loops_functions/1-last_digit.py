ccfc3bb73076e2eb672def97a434ac3a9c2226d4
DestinedCodes <yiradesaf@gmail.com>
2023-08-10 12:59:50 +0100

    added int formatter and converted negative nums

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = number % 10
-print(f"Last digit of {number} is {last_digit}", end=" ")
+if number < 0:
+    last_digit = -number % 10
+else:
+    last_digit = number % 10
+
+print(f"Last digit of {number:d} is {last_digit:d}", end=" ")
 
 if last_digit > 5:
     print("and is greater than 5")
