5cbcda028856ea3ad6c80409566a35136ecad9d8
DestinedCodes <yiradesaf@gmail.com>
2023-08-10 13:06:30 +0100

    removed conversion for negative numbers: misbehaves locslly

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number < 0:
-    last_digit = (number * -1) % 10
-else:
-    last_digit = number % 10
+last_digit = number % 10
 
 print(f"Last digit of {number:d} is {last_digit:d}", end=" ")
 
