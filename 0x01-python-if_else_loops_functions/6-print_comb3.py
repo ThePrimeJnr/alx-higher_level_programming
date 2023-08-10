8d3de619f66d9f18cefb29124455b7ae7c13a178
DestinedCodes <yiradesaf@gmail.com>
2023-08-10 20:13:57 +0100

    fix pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(0, 89):
-    if (i % 10 > i/ 10):
-       print("{:02d}".format(i), end=", ")
+    if (i % 10 > i / 10):
+        print("{:02d}".format(i), end=", ")
 print("89")
