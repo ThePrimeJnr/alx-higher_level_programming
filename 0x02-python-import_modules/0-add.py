a60a2c5f0d3150ce404ef5c096c6e83961d7b97d
DestinedCodes <yiradesat@gmail.com>
2023-08-11 17:05:52 +0100

    only import is under the condition if name is main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__":
     import add_0
 
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    result = add_0.add(a, b)
+result = add_0.add(a, b)
 
-    print("{} + {} = {}".format(a, b, result))
+print("{} + {} = {}".format(a, b, result))
