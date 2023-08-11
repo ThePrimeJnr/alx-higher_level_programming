f4d6e60e1967e80728dd1c85720ea599dfa6f61a
DestinedCodes <yiradesat@gmail.com>
2023-08-11 17:00:25 +0100

    Add condition if name is __import__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-import add_0
+if __name__ is not __import__:
+    import add_0
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-result = add_0.add(a, b)
+    result = add_0.add(a, b)
 
-print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, result))
