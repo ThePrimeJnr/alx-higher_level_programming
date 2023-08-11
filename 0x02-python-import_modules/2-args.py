35030541ebf21cc866499b611e88d641f9c1b1f5
DestinedCodes <yiradesat@gmail.com>
2023-08-11 21:52:16 +0100

    fix typo in 2 args program

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
 length = len(argv)
 
 if length == 1:
-    print("0 argument.")
+    print("0 arguments.")
 elif length == 2:
-    print("{} arguments:".format(length - 1))
-    print("1: {}".format(argv[1]))
+    print("1 argument:")
+    print("1: {:s}".format(argv[1]))
 else:
-    print("{} arguments:".format(length - 1))
+    print("{:d} arguments:".format(length - 1))
     for i in range(1, length):
-        print("{}: {}".format(i, argv[i]))
+        print("{:d}: {:s}".format(i, argv[i]))
