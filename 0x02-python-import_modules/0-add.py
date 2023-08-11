0d06698c33056ca1c5f507c38f362d2033310146
DestinedCodes <yiradesat@gmail.com>
2023-08-11 17:08:31 +0100

    add data type to string format

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
 
 a = 1
 b = 2
 
-result = add_0.add(a, b)
+result = add(a, b)
 
 print("{:d} + {:d} = {:d}".format(a, b, result))
