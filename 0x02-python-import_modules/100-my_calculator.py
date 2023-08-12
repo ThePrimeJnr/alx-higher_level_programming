9ba1c90bb488e795b64712406c1e16aea12d511b
DestinedCodes <yiradesat@gmail.com>
2023-08-12 04:17:26 +0100

    added only the needed function and refactored a bit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,24 +2,24 @@
 
 if __name__ == "__main__":
     from sys import argv
-    import calculator_1 as cal
 
     if len(argv) != 4:
         print("usage: {:s} <a> <operator> <b>".format(argv[0]))
         exit(1)
 
+    from calculator_1 import add, sub, mul, div
     a = int(argv[1])
     b = int(argv[3])
-    operator = str(argv[2])
+    oper = str(argv[2])
 
-    if operator == "+":
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, cal.add(a, b)))
-    elif operator == "-":
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, cal.sub(a, b)))
-    elif operator == "*":
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, cal.mul(a, b)))
-    elif operator == "/":
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, cal.div(a, b)))
+    if oper == "+":
+        print("{} {} {} = {}".format(a, oper, b, add(a, b)))
+    elif oper == "-":
+        print("{} {} {} = {}".format(a, oper, b, add(a, b)))
+    elif oper == "*":
+        print("{} {} {} = {}".format(a, oper, b, add(a, b)))
+    elif oper == "/":
+        print("{} {} {} = {}".format(a, oper, b, add(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
