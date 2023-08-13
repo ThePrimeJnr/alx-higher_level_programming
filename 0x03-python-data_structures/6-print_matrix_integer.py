2477585164744b7fe4a98496a8e10683bef4ad27
DestinedCodes <yiradesat@gmail.com>
2023-08-13 17:57:30 +0100

    remove space for last digit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for cell in row:
-            print("{:d}".format(cell), end=" ")
+            print("{:d}".format(cell), end=" " if cell != row[-1] else "")
         print("")
