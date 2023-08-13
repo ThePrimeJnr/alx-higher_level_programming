88ab8d934d68f94cff3fd8d20594f7b435b8b2c2
DestinedCodes <yiradesat@gmail.com>
2023-08-13 17:09:37 +0100

    use reversed function instead and remove integer format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    i = len(my_list)
-    while i >= 0:
-        i -= 1
-        print("{:d}".format(my_list[i]))
+    for element in reversed(my_list):
+        print("{}".format(element))
