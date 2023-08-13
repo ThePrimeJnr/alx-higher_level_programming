a3e76aa9fd8c6556fedde68b4e09661f83a65858
DestinedCodes <yiradesat@gmail.com>
2023-08-13 18:29:02 +0100

    return 0 for len and none as string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,6 @@
 
 
 def multiple_returns(sentence):
-    length = len(sentence)
-
-    if length == 0:
-        return (None, "")
-    else:
-        return (length, sentence[0])
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
