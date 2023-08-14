5a4d6866f3e136019fba3f85191a1970250a8ba5
DestinedCodes <yiradesat@gmail.com>
2023-08-14 16:52:14 +0100

    switch back to del added edge cases

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,7 @@
 def delete_at(my_list=[], idx=0):
     if not my_list:
         return None
-
-    my_list.pop(idx)
+    if idx < 0 or idx >= len(my_list):
+        return None
+    del my_list[idx]
     return my_list
