21ea914eca076e42b5570a02d607fcf3f796ba96
DestinedCodes <yiradesat@gmail.com>
2023-08-14 16:55:45 +0100

    return my_list instead of none

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,8 @@
 
 def delete_at(my_list=[], idx=0):
     if not my_list:
-        return None
+        return my_list
     if idx < 0 or idx >= len(my_list):
-        return None
+        return my_list
     del my_list[idx]
     return my_list
