d71ff4d639aead368b431e58e97e3ecd62a14016
DestinedCodes <yiradesat@gmail.com>
2023-08-30 21:10:20 +0100

    use finnaly save value in result then append in finally section2

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,15 +4,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
+        result = 0
         try:
-            new_list.append(my_list_1[i] / my_list_2[i])
+            result = my_list_1[i] / my_list_2[i]
         except (ValueError, TypeError):
             print("wrong type")
-            new_list.append(0)
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
         except IndexError:
             print("out of range")
-            break
+        finally:
+            new_list.append(result)
     return new_list
