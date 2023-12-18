6c521c35de0f32d523988b6b126c60767aa41496
Destiny Saturday <yiradesat@gmail.com>
2023-12-18 07:49:29 +0100

    Fix import statement in test file

--- 0x05-python-exceptions/tests/0-main.py
+++ 0x05-python-exceptions/tests/0-main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-safe_print_list = __import__('../0-safe_print_list').safe_print_list
+
+from sys import path
+
+path.append("..")
+safe_print_list = __import__("0-safe_print_list").safe_print_list
 
 my_list = [1, 2, 3, 4, 5]
 
