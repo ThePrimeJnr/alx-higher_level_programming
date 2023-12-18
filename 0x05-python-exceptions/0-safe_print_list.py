3b472e2c3bb712902d9836dd0a27a3d44432ab6d
Destiny Saturday <yiradesat@gmail.com>
2023-12-18 07:05:12 +0100

    Added pylintrc and Sfeprint function

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """A function that prints x elements of a list."""
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            x = i
+            break
+    print()
+    return x
