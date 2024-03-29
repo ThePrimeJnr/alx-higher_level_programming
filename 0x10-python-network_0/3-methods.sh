5b44fc1ab2fef7b390da47566958724f9ecf3dd4
Destiny Saturday <yiradesat@gmail.com>
2024-03-29 02:55:41 +0100

    fix: I am lost :(

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script to print the methods allowed in a server
-curl -s -i -X OPTIONS "$1" | grep "Allow:" | cut -d ":" -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
