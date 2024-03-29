8a65e344c618beb026e3bcf925ae0071e427046a
Destiny Saturday <yiradesat@gmail.com>
2024-03-29 01:31:11 +0100

    fix: add -L flag to follow redirection

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send GET request to the URL and capture response body
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" = "200" ] && curl -s "$1"
+response=$(curl -s -o /dev/null -w "%{http_code}" -L "$1"); [ "$response" = "200" ] && curl -s -L "$1"
